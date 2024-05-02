20d351db3c16993c917fcb04e4d9c580390b1616
ImJordanNetz <jonetz@eastsideprep.org>
2024-05-02 00:06:15 -0700

    Added some comments to code

--- tendertouch/js/index.js
+++ tendertouch/js/index.js
@@ -1,28 +1,24 @@
 function filterDivs() {
-    // Retrieve the input text and convert it to lowercase for case-insensitive matching
     var inputText = document.getElementById('filterInput').value.toLowerCase();
 
-    // Get all divs with class 'care-guide-item'
     var guideItems = document.querySelectorAll('.care-guide-item');
 
-    // Assume no matches found initially
     let found = false;
 
     guideItems.forEach(function(div) {
-        // Normalize the div's ID and check if it includes the normalized input text
-        if (div.id.toLowerCase().includes(inputText.replace(/\s+/g, ''))) {
-            div.style.display = 'block'; // Show the div
-            found = true; // At least one match was found
+        if (div.id.toLowerCase().includes(inputText.replace(/\s+/g, ''))) { //removes space: https://flaviocopes.com/how-to-replace-whitespace-javascript/
+            div.style.display = 'block'; 
+            found = true; 
         } else {
-            div.style.display = 'none'; // Hide the div
+            div.style.display = 'none'; 
         }
     });
 
-    // Handle the case where no matching guides are found
+
     var noGuidesDiv = document.getElementById('none-found');
     if (found) {
-        noGuidesDiv.style.display = 'none'; // Hide the 'No Guides Found' div if matches are found
+        noGuidesDiv.style.display = 'none'; 
     } else {
-        noGuidesDiv.style.display = 'block'; // Show the 'No Guides Found' div if no matches are found
+        noGuidesDiv.style.display = 'block'; 
     }
 }
